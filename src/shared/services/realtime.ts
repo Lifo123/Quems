000394d3b08f6c891a213eb7404c443157d292e7
Lifo123 <alberto_p-@hotmail.com>
2025-12-09 19:10:04 -0500

    Removing creds

--- src/shared/services/realtime.ts
+++ src/shared/services/realtime.ts
@@ -3,13 +3,19 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
+const API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const AUTH_DOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN;
+const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const STORAGE_BUCKET = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDqoH9U9yfWbJbSXxiOyxy-Wu3ddRYchv8",
-    authDomain: "quems-2f307.firebaseapp.com",
-    projectId: "quems-2f307",
-    storageBucket: "quems-2f307.firebasestorage.app",
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
     messagingSenderId: "289358159470",
-    appId: "1:289358159470:web:6710e12f88c3fb1575507f",
+    appId: APP_ID,
     measurementId: "G-B0TE5NXJ4J"
 };
 
